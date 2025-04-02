f683d2e972763133f9494883a0776e36235f8890
Ronald Huidrom <ronhuidrom@gmail.com>
2025-04-03 00:35:10 +0530

    Fixed many issues, rewrote src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use rusqlite::{Connection, Result};
+use serde::Serialize;
+use anyhow::Result as EResult;
+
+#[derive(Debug, Serialize)]
+struct MyRow {
+    title: String,
+    author: String,
+}
+
+fn main() -> EResult<()> {
+    let conn = Connection::open("/home/ronald/Documents/mylibrary.db")?;
+
+    let mut stmt = conn.prepare(
+	"SELECT title, author FROM everymans_library_fiction"
+    )?;
+
+    let rows = stmt.query_map([], |row| {
+	Ok(MyRow {
+	    title: row.get(0)?,
+	    author: row.get(1)?,
+	})
+    });
+
+    let rows = rows?;
+
+    for row_result in rows {
+	let row = row_result?;
+	println!("{:#?}", row)
+    }
+
+    Ok(())
 }
